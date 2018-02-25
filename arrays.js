5d4c530fb021806872f3821d6d24fa6a9856c0a1
Bryan Williams <brywilldev@gmail.com>
2018-02-25 19:04:35 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -20,3 +20,25 @@ function addElementToEndOfArray(arr, el) {
 function destructivelyAddElementToEndOfArray(arr, el) {
   return arr.push(el)
 }
+
+function accessElementInArray(arr, i) {
+  return arr[i]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(arr) {
+  return arr.shift
+}
+
+function removeElementFromBeginningOfArray(arr) {
+  arr = arr.slice(1)
+  return arr
+}
+
+function destructivelyRemoveElementFromEndOfArray(arr) {
+  arr.pop()
+  return arr
+}
+
+function removeElementFromEndOfArray(arr) {
+  return arr.slice(0, slice.length - 1)
+}
